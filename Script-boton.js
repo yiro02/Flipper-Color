d973ea4edd901a4ea00224f25dc296d95445adaa
Tonin Santos <yiro7261@gmail.com>
2025-03-30 23:19:13 -0400

    Added The Maquetacion of the Project

--- Script-boton.js
+++ Script-boton.js
@@ -12,9 +12,9 @@
 function changeColor() {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 250);
-  let blue = Math.floor(Math.random() * 250);
+  let blue = Math.floor(Math.random() * 256);
   let coral = Math.floor(Math.random() * 250);
-  let magenta = Math.floor(Math.random() * 256);
+  let magenta = Math.floor(Math.random() * 156);
 
   let color =
     "rgb(" + red + "," + green + "," + blue + "," + coral + magenta + ")";
