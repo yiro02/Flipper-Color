35cde692e2ebd83879d6f23a7ed529a1d8c1d19f
Tonin Santos <yiro7261@gmail.com>
2025-03-30 22:36:51 -0400

    Second-Commit

---
+++ Script-boton.js
@@ -0,0 +1,24 @@
+/*function btn() {
+  let red = Math.floor(Math.random() * 250);
+  let blue = Math.floor(Math.random() * 256);
+  let gray = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let coral = Math.floor(Math.random() * 256);
+  let color =
+    "rgb(" + red + "," + blue + "," + gray + "," + green + "," + coral + ")";
+  document.getElementById("colorFlipper").style.backgroundColor = color;
+}
+*/
+function changeColor() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 250);
+  let blue = Math.floor(Math.random() * 250);
+  let coral = Math.floor(Math.random() * 250);
+  let magenta = Math.floor(Math.random() * 256);
+
+  let color =
+    "rgb(" + red + "," + green + "," + blue + "," + coral + magenta + ")";
+  document.getElementById("colorFlipper").style.backgroundColor = color;
+}
+
+alert("klk, pariguayo");
